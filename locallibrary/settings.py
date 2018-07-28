173ba4a0e3f9d904d16e2c3749d45dc996ed59f9
aditya81070 <aditya81070@gmail.com>
2018-07-28 23:39:56 +0530

    requirement updated

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -124,3 +124,13 @@ USE_TZ = True
 
 LOGIN_REDIRECT_URL='/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

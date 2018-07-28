9e560ef016ca01495e27c2226847d8c8300d90d6
aditya81070 <aditya81070@gmail.com>
2018-07-28 22:39:08 +0530

    settings changed

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ea1r2+&wo&$@@5*(eqkrz^@s_(#nh7tb%rp#6j_yntfu*+e1t8'
+# SECRET_KEY = 'ea1r2+&wo&$@@5*(eqkrz^@s_(#nh7tb%rp#6j_yntfu*+e1t8'
+SECRET_KEY=os.environ.get('DJANGO_SECRET_KEY','ea1r2+&wo&$@@5*(eqkrz^@s_(#nh7tb%rp#6j_yntfu*+e1t8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
@@ -121,20 +124,3 @@ USE_TZ = True
 
 LOGIN_REDIRECT_URL='/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-# The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
